de9820d0b279ca2ed3aec11c9e5c8776be81ac06
mjkim <mjkim@woowahan.com>
2024-03-13 14:51:58 +0900

    add debian packaging script
    BUG=RLAB-2041
    TEST=ros2 , Ubuntu 22.04.4 LTS, ThinkPad-X1-Extreme-Gen-4i

---
+++ debian/shlibs.local.ex
@@ -0,0 +1,1 @@
+libvalijson 1.0.1 valijson (>> 1.0.1-0), valijson (<< 1.0.1-99)
---
+++ debian/valijson.doc-base.ex
@@ -0,0 +1,20 @@
+Document: valijson
+Title: Debian valijson Manual
+Author: <insert document author here>
+Abstract: This manual describes what valijson is
+ and how it can be used to
+ manage online manuals on Debian systems.
+Section: unknown
+
+Format: debiandoc-sgml
+Files: /usr/share/doc/valijson/valijson.sgml.gz
+
+Format: postscript
+Files: /usr/share/doc/valijson/valijson.ps.gz
+
+Format: text
+Files: /usr/share/doc/valijson/valijson.text.gz
+
+Format: HTML
+Index: /usr/share/doc/valijson/html/index.html
+Files: /usr/share/doc/valijson/html/*.html
